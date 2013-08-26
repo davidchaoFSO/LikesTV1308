7d5367a0d1775aba0e1ce91188476a72222bf713
David Chao <davidchao@fullsail.edu>
2013-08-26 03:02:19 -0500

    Updated DB configs to match pagoda variables

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -10,12 +10,12 @@ return array(
 'default' => array(
 		'type'			=> 'mysql',
 		'connection'	=> array(
-			'hostname'		=> '127.0.0.1',
-			'username'		=> 'root',
-			'password'		=> 'root',
-			'database'		=> 'ltvprofile',
+			'hostname'		=> $_SERVER['DB1_HOST'],
+			'username'		=> $_SERVER['DB1_USER'],
+			'password'		=> $_SERVER['DB1_PASS'],
+			'database'		=> $_SERVER['DB1_NAME'],
 			'persistent'	=> false,
-			'port'			=> '8889'
+			'port'			=> $_SERVER['DB1_PORT']
 		),
 		'identifier'	=> '`',
 		'table_prefix'	=> '',
